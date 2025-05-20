bde7242f7aedf8606e5908e6f453eee4ec0998f2
Vasanth-070 <vasanthkzoomin@gmail.com>
2025-05-21 00:25:34 +0530

    doc

--- lib/interfaces/game_mode.dart
+++ lib/interfaces/game_mode.dart
@@ -1,18 +1,18 @@
 import 'package:cricket_card_game/interfaces/result.dart';
 
 abstract class Mode {
-  double get opponentDamage;
-  double get activePlayerDamage;
-}
-
-abstract class RoundLevelMode implements Mode {
   /// this is the damage dealt to opponents, if the active
   /// player wins the comparison.
-  @override
   double get opponentDamage;
 
   /// this is the damage dealt to the active player, if the
   /// opponent wins the comparison.
+  double get activePlayerDamage;
+}
+
+abstract class RoundLevelMode implements Mode {
+  @override
+  double get opponentDamage;
   @override
   double get activePlayerDamage;
 
